15f529440f039ae900db91b7b8e2367fb6296e99
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-10 16:07:55 +0530

    Adding category

---
+++ solution.js
@@ -0,0 +1,7 @@
+const task = document.querySelector('#task').value
+const category = document.querySelector('#category').value
+const button = document.querySelector('#submit')
+
+button.addEventListener('click', (e) => {
+    
+})

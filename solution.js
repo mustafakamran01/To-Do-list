f7fa962db413cc1be548f6c0004a8e665349b4cb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-10 16:34:07 +0530

    UrgentList

--- solution.js
+++ solution.js
@@ -1,7 +1,20 @@
-const task = document.querySelector('#task').value
-const category = document.querySelector('#category').value
 const button = document.querySelector('#submit')
 
 button.addEventListener('click', (e) => {
-    
+    const task = document.querySelector('#task').value
+    const category = (document.querySelector('#category').value).toLowerCase()
+    const urgentUL = document.querySelector('#urgent')
+    const notUrgentUL = document.querySelector('#notUrgent')
+
+    if (category === "urgent") {
+        let myLi = document.createElement('li')
+        myLi.appendChild(document.createTextNode(task))
+        urgentUL.appendChild(myLi)
+    }
+
+    if (category === "noturgent") {
+        let myLi = document.createElement('li')
+        myLi.appendChild(document.createTextNode(task))
+        notUrgentUL.appendChild(myLi)
+    }
 })
